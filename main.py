44b4b5b054946e83555cd105b007d09784d443e6
sjy <936694123@qq.com>
2025-03-12 10:23:15 +0800

    zzc

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ class SuperResolutionNet(nn.Module):
         out = self.relu(self.conv2(out))
         out = self.conv3(out)
         return out
-
+# dabendai
+#wbushi dacanmao a 
 # Download checkpoint and test image
 # urls = ['https://download.openmmlab.com/mmediting/restorers/srcnn/srcnn_x4k915_1x16_1000k_div2k_20200608-4186f232.pth',
 #     'https://raw.githubusercontent.com/open-mmlab/mmediting/master/tests/data/face/000001.png']
